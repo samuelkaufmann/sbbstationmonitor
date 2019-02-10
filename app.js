1c9e993b40920956fcd5646dd7cb1e6be14f394b
Samuel Kaufmann <samuelkaufmann@Samuels-MacBook-Pro.local>
2019-02-10 21:08:50 +0100

    fetching real data from public api for a single station and for one next connection

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var syncClient = require('sync-rest-client');
 var app = express();
 app.set('view engine','ejs');
 
@@ -10,20 +11,38 @@ app.get('', function (req, res) {
 })
 //ROUTES API
 app.get('/api/stationmonitor/:stationid/:limit', function (req, res) {
-	var stationid = 8571216;
-	var limit = 1;
+	//var stationid = 8571216;
+	//var limit = 1;
 	stationid = req.params.stationid;
 	limit = req.params.limit;
-	//var apiResponse = getsbbdata(stationid, limit);
-	res.render('api/index', {person: stationid + limit});
+	var url = 'http://transport.opendata.ch/v1/stationboard?id=' + stationid + '&limit=' + limit;
+	var result = syncClient.get(url);
+	var body = result.body;
+	var stationname = body.station.name;
+	var stops = body.stationboard;
+	var departureTime;
+	var departureTo;
+	var departureDelay;
+	var departures;
+	for(var x = 0;x<stops.length;x++) {
+		departureTime = stops[x].stop.departureTimestamp,
+		departureTo = stops[x].to,
+		departureDelay = stops[x].stop.delay
+		var timestamp = departureTime;
+		var date = new Date(timestamp * 1000);
+		departureTime = date.getHours() + ':' + date.getMinutes();
+		var departures = {
+			departureTime: departureTime,
+			to: departureTo,
+			delay: departureDelay
+		}
+	}
+	var datas = {
+		stationsname: stationname,
+		departures: departures
+	}
+	res.send(datas);
 })
-
-function getsbbdata(stationid, limit) {
-	var requrl = 'http://transport.opendata.ch/v1/stationboard?id=' + stationid + '&limit=' + limit;
-	var connectionData;
-	connectionData = requrl;
-	return connectionData;
-}
 
 var server = app.listen(8081, function () {
 	var host = server.address().address
